fdd18ed7b3743c339b8142b86a9130e6e2f39f60
David Lares <david.e.lares@gmail.com>
2018-12-20 11:33:20 -0400

    Resolving /user GETRequest

--- server.go
+++ server.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-  "fmt"
+  "log"
   "net/http"
   "github.com/gorilla/mux"
 )
 
 func main(){
-  fmt.Println("Hello, World")
+  // fmt.Println("Hello, World")
+  r := mux.NewRouter()
+  // handling URLs
+  r.HandleFunc("/user/", GetUser).Methods("GET")
+  log.Println("Server running on port 8000")
+  log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func GetUser(w http.ResponseWriter, r* http.Request){
+  w.Write([]byte("Gorilla \n"))
 }
