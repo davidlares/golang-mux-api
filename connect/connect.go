36cd0117518e6d7b206a5cbda6a2300dec1ecd8c
David Lares <david.e.lares@gmail.com>
2018-12-20 12:30:01 -0400

    DB Integration Package and SQL users table

---
+++ connect/connect.go
@@ -0,0 +1,39 @@
+package connect
+
+import (
+  "log"
+  "github.com/jinzhu/gorm"
+ _ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var connection *gorm.DB
+const engine_sql string = "mysql"
+
+// should be secrets or ENV variables
+const username string = "root"
+const password string = ""
+const database string = "go_api"
+
+func InitializeDatabase(){
+  connection = ConnectORM(CreateString())
+  log.Println("DB Connection successfully created")
+}
+
+func CloseConnection(){
+  connection.Close()
+  log.Println("DB Connection successfully closed")
+}
+
+// establishing ORM connection with MySQL
+func ConnectORM(stringConnection string) *gorm.DB {
+  connection, err := gorm.Open(engine_sql, stringConnection)
+  if err != nil{
+    log.Fatal(err)
+    return nil
+  }
+  return connection
+}
+
+func CreateString() string {
+  return username + ":" + password + "@/" + database
+}
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
   "net/http"
   "github.com/gorilla/mux"
   "encoding/json"
+  "./connect"
 )
 
 // JSON Struct
@@ -15,15 +16,23 @@ type User struct {
 }
 
 func main(){
+  // DB connection
+  connect.InitializeDatabase()
+  defer connect.CloseConnection()
+
   // fmt.Println("Hello, World")
   r := mux.NewRouter()
   // handling URLs
-  r.HandleFunc("/user/", GetUser).Methods("GET")
+  r.HandleFunc("/user/{id}", GetUser).Methods("GET")
   log.Println("Server running on port 8000")
   log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func GetUser(w http.ResponseWriter, r* http.Request){
+  // getting ID
+  // vars := mux.Vars(r)
+  // user_id := vars["id"]
+
   // w.Write([]byte("Gorilla \n"))
   user := User {"davidlares", "David","Lares"}
   // converting struct into response
